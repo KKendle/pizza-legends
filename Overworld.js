cce6065429279c475629d1c9d8f8df902de53dd1
Kevin Kendle <1066413+KKendle@users.noreply.github.com>
2022-02-27 17:31:54 -0600

    adding game objects

--- Overworld.js
+++ Overworld.js
@@ -10,40 +10,22 @@ class Overworld {
     image.onload = () => {
       this.ctx.drawImage(image, 0, 0);
     }
-    image.src = '/images/maps/DemoLower.png'
+    image.src = '/images/maps/DemoLower.png';
 
-    const x = 5;
-    const y = 6;
-    const shadow = new Image();
-    shadow.onload = () => {
-      this.ctx.drawImage(
-        shadow,
-        0, // left cut
-        0, // top cut
-        32, // width cut
-        32, // height cut
-        x * 16 - 8, // x position on map
-        y * 16 - 18, // y position on map
-        32, // draw cut width
-        32 // draw cut height
-      );
-    }
-    shadow.src = '/images/characters/shadow.png'
+    // place some game objects
+    const hero = new GameObject({
+      x: 5,
+      y: 6
+    });
+    const npc1 = new GameObject({
+      x: 7,
+      y: 9,
+      src: '/images/characters/people/npc1.png'
+    });
 
-    const hero = new Image();
-    hero.onload = () => {
-      this.ctx.drawImage(
-        hero,
-        0, // left cut
-        0, // top cut
-        32, // width cut
-        32, // height cut
-        x * 16 - 8, // x position on map
-        y * 16 - 18, // y position on map
-        32, // draw cut width
-        32 // draw cut height
-      );
-    }
-    hero.src = '/images/characters/people/hero.png'
+    setTimeout(() => {
+      hero.sprite.draw(this.ctx);
+      npc1.sprite.draw(this.ctx);
+    }, 200)
   }
 }
